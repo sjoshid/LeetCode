a0160da52892b35ab1f752045601472a38e40471
joshi.sujit020386@gmail.com <ILGitHub2>
2017-11-17 21:28:57 -0500

    Factorial solution

---
+++ src/com/practice/Factorial.java
@@ -0,0 +1,28 @@
+package com.practice;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        System.out.println(f.getFactorial(6));
+    }
+
+    public int getFactorial(int i){
+
+        if(i == 1)
+            return 1;
+
+        return i * getFactorial(i - 1);
+    }
+
+    public int getFactorial1(int i) {
+        int cfact = i--;
+
+        while(i > 0){
+            cfact = cfact * i;
+            i--;
+        }
+        System.out.println(cfact);
+        return cfact;
+    }
+}
