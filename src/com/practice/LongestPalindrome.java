b70c3f8820d58fecbcc300a60f46447723515a2f
Sujit Joshi <joshi.sujit020386@gmail.com>
2018-01-17 21:13:34 -0500

    Solution to https://leetcode.com/problems/longest-palindromic-substring/description/

---
+++ src/com/practice/LongestPalindrome.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+/*
+    https://leetcode.com/problems/longest-palindromic-substring/description/
+ */
+public class LongestPalindrome {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
