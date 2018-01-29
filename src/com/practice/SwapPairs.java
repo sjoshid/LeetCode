978f3b9fd7cd73772272cae7288b38b14dc901cd
joshi.sujit020386@gmail.com <ILGitHub2>
2018-01-28 20:47:50 -0500

    SOlution for LeetCode https://leetcode.com/problems/swap-nodes-in-pairs/description/

--- src/com/practice/SwapPairs.java
+++ src/com/practice/SwapPairs.java
@@ -2,29 +2,65 @@ package com.practice;
 
 import com.practice.common.ListNode;
 
+/**
+ * Solution for https://leetcode.com/problems/swap-nodes-in-pairs/description/
+ */
 public class SwapPairs {
 
     public static void main(String[] args) {
 
+        SwapPairs sp = new SwapPairs();
+        ListNode<Integer> h = new ListNode<>(1);
+        ListNode<Integer> a = new ListNode<>(2);
+        ListNode<Integer> b = new ListNode<>(3);
+        ListNode<Integer> c = new ListNode<>(4);
+        ListNode<Integer> d = new ListNode<>(5);
+        ListNode<Integer> e = new ListNode<>(6);
+
+        h.next = a;
+        a.next = b;
+        /*b.next = c;
+        c.next = d;
+        d.next = e;*/
+
+        ListNode<Integer> r = sp.swapPairs(h);
+
+        while (r != null) {
+            System.out.println(r.val);
+            r = r.next;
+        }
+
     }
 
     public ListNode swapPairs(ListNode<Integer> head) {
-        ListNode<Integer> returnMe = head;
 
-        while(head != null) {
-            ListNode<Integer> f = head;
-            ListNode<Integer> s = f.next;
-            ListNode<Integer> nf = null;
-            if(s != null) {
-                nf = s.next;
+        if (head != null) {
+            if(head.next != null) {
+                ListNode<Integer> f = head;
+                ListNode<Integer> returnMe = head.next, s = head.next;
 
-                s.next = f;
-                f.next = nf;
+                while (s != null) {
+                    ListNode<Integer> nf = s.next;
+
+                    s.next = f;
+                    if (nf != null) {
+                        if(nf.next != null) {
+                            f.next = nf.next;
+                        } else {
+                            f.next = nf;
+                        }
+                        f = nf;
+                        s = f.next;
+                    } else {
+                        f.next = null;
+                        s = null;
+                    }
+                }
+                return returnMe;
             }
-
-            head = nf;
+            return head;
         }
-
-        return returnMe;
+        return null;
     }
 }
+
