ba4f8a0347c505197d6102de074dd7b5807658e7
Sugs <joshi.sujit020386@gmail.com>
2018-02-04 16:40:35 -0500

    Refactoring https://leetcode.com/problems/wildcard-matching/description/

--- src/com/practice/WildcardMatching.java
+++ src/com/practice/WildcardMatching.java
@@ -17,18 +17,19 @@ public class WildcardMatching {
 
             switch (pc) {
                 case '*':
-                    char nonStartChar = firstNonStartChar(p, pi);
+                    NonStartDetails nonStartDetails = firstNonStartChar(p, pi);
 
-                    if(nonStartChar == '?') {
-                        pi = p.indexOf('?', pi);
-                    } else if (nonStartChar != ' ') {
+                    if(nonStartDetails.nonStarChar == '?') {
+                        pi = nonStartDetails.index;
+                    } else if (nonStartDetails.nonStarChar != ' ') {
                         String subStringFromS = s.substring(si, s.length() - 1);
-                        int index = starFound(subStringFromS, nonStartChar);
+                        int index = starFound(subStringFromS, nonStartDetails.nonStarChar);
 
                         if (index == -1) {
                             return false;
                         } else {
                             si = index;
+                            pi = nonStartDetails.index;
                         }
                     }
                     break;
@@ -53,12 +54,22 @@ public class WildcardMatching {
         return subStringFromS.indexOf(charFromP);
     }
 
-    private char firstNonStartChar(String s, int startIndex) {
+    private NonStartDetails firstNonStartChar(String s, int startIndex) {
         for (int i = startIndex; i < s.length(); i++) {
             if(s.charAt(i) != '*') {
-                return s.charAt(i);
+                return new NonStartDetails(i, s.charAt(i));
             }
         }
-        return ' ';
+        return new NonStartDetails(-1, ' ');
+    }
+}
+
+class NonStartDetails {
+    int index;
+    char nonStarChar;
+
+    public NonStartDetails(int index, char nonStarChar){
+        this.index = index;
+        this.nonStarChar = nonStarChar;
     }
 }
