14af79079a9c71edb9e57e56f80793da968e111b
Sugs <joshi.sujit020386@gmail.com>
2018-02-04 16:29:12 -0500

    WIP for https://leetcode.com/problems/wildcard-matching/description/

---
+++ src/com/practice/WildcardMatching.java
@@ -0,0 +1,64 @@
+package com.practice;
+
+public class WildcardMatching {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isMatch(String s, String p) {
+
+        int si = 0;
+        int pi = 0;
+
+        while(si < s.length() && pi < p.length()){
+            char sc = s.charAt(si);
+            char pc = p.charAt(pi);
+
+            switch (pc) {
+                case '*':
+                    char charFromP = firstNonStartChar(p, pi);
+
+                    if(charFromP == '?') {
+                        pi = p.indexOf('?', pi);
+                    } else if (charFromP != ' ') {
+                        String subStringFromS = s.substring(si, s.length() - 1);
+                        int index = starFound(subStringFromS, charFromP);
+
+                        if (index == -1) {
+                            return false;
+                        } else {
+                            si = index;
+                        }
+                    }
+                    break;
+                case '?':
+                    //do nothing.
+                    break;
+                default:
+                    if (!charFound(sc, pc)) return false;
+            }
+
+            si++;
+            pi++;
+        }
+        return true;
+    }
+
+    private boolean charFound (char s, char p) {
+        return s == p;
+    }
+
+    private int starFound (String subStringFromS, char charFromP) {
+        return subStringFromS.indexOf(charFromP);
+    }
+
+    private char firstNonStartChar(String s, int startIndex) {
+        for (int i = startIndex; i < s.length(); i++) {
+            if(s.charAt(i) != '*') {
+                return s.charAt(i);
+            }
+        }
+        return ' ';
+    }
+}
