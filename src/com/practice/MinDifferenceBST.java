6c115cbb131416ccee6e69396ec4ab3580eabac7
Sujit Joshi <joshi.sujit020386@gmail.com>
2018-02-18 12:39:21 -0500

    Solution to https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/

---
+++ src/com/practice/MinDifferenceBST.java
@@ -0,0 +1,31 @@
+package com.practice;
+
+import com.practice.common.BSTNode;
+
+/**
+ * https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/
+ */
+public class MinDifferenceBST {
+
+    Integer prev;
+    Integer min = Integer.MAX_VALUE;
+
+    public int minDiffInBST(BSTNode<Integer> root) {
+        minDiff(root);
+        return min;
+    }
+
+    public void minDiff(BSTNode<Integer> root) {
+        if (root != null) {
+            minDiff(root.getLeft());
+            if(prev == null) {
+                prev = root.getValue();
+            } else {
+                min = Math.min(min, root.getValue() - prev);
+                prev = root.getValue();
+            }
+
+            minDiff(root.getRight());
+        }
+    }
+}
